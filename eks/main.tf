273446bec62ed0e3e6f91f4354a8835e7193823f
Reza Chalak <mrezachalak@gmail.com>
2023-05-13 21:36:40 +0330

    simpler mode

--- eks/main.tf
+++ eks/main.tf
@@ -1,4 +1,4 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+module "eks_example_self_managed_node_group" {
+  source  = "terraform-aws-modules/eks/aws//examples/self_managed_node_group"
   version = "19.13.1"
 }
