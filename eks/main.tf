dd39202f6eb74496689daafc0e07d486043ffc27
Reza Chalak <mrezachalak@gmail.com>
2023-05-15 00:00:20 +0330

    fix

--- eks/main.tf
+++ eks/main.tf
@@ -1,4 +1,4 @@
 module "eks_example_self_managed_node_group" {
-  source  = "terraform-aws-modules/eks/aws//examples/self_managed_node_group"
+  source  = "terraform-aws-modules/eks/aws//examples/eks_managed_node_group"
   version = "19.13.1"
 }
